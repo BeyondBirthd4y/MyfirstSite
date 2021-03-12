c9ecbcecff0e7034e1248130e9021105dca6e042
Gabriel Fernandes <72037743+BeyondBirthd4y@users.noreply.github.com>
2021-03-12 16:13:57 -0300

    Add files via upload
    Criei este site com um pequeno script, com objetivo de treinar adição de audio à páginas HTML

---
+++ script.js
@@ -0,0 +1,7 @@
+function change() {
+    let song = document.getElementById("character")
+    var vamp = document.getElementById("vampire")
+    vamp.src = "Dracula.png"
+    var audio = new Audio('CTtheme.mp3')
+    audio.play()
+}
